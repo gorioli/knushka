7739bf18b79086aa03fe48de0b2d27cf644a77df
Olga-Go at knushka.com <olga@knushka.com>
2020-03-02 22:58:30 -0600

    Add initial site

---
+++ README.md
@@ -0,0 +1,7 @@
+**knushka.com** home page
+
+https://gorioli.github.io/knushka/
+
+Older version is:
+
+http://knushka.com/
---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+document.querySelector('.navbar-collapse').addEventListener('click', () => {
+    document.querySelector('.navbar-toggle').click();
+});
